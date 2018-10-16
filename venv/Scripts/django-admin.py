012d3c430f3f74a2421d7791dde3e4234aac17dd
anudeepvenkat <42250047+anudeepvenkat@users.noreply.github.com>
2018-10-16 16:44:53 +0530

    Initial commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\sphadke\documents\snigdha_study\fi\payroll\django-master\django-master\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
